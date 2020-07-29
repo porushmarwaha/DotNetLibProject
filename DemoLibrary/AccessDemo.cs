6415056f896222c4f811014ee3385b9ace717b57
porushmarwaha <porushmarwaha@gmail.com>
2020-07-29 14:20:01 +0530

    New classes added  in project dev code

---
+++ DemoLibrary/AccessDemo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DemoLibrary
+{
+    public  class AccessLibrary
+    {
+        private void PrivateDemo()
+        {
+            
+        }
+
+        private protected void PrivateProtectedDemo()
+        {
+
+        }
+
+        protected void ProtectedDemo()
+        {
+
+        }
+
+        protected internal void ProtectedInternalDemo()
+        {
+
+        }
+
+        internal void InternalDemo()
+        {
+
+        }
+
+        public void PublicDemo()
+        {
+
+        }
+    }
+}
---
+++ DemoLibrary/ActiveDemo.cs
@@ -0,0 +1,9 @@
+namespace DemoLibrary
+{
+    public class ActiveDemo
+    {
+               
+               
+            
+    }
+}
