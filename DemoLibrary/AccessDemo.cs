427607b40ac3672154ceace676a6c76cc539116c
porushmarwaha <porushmarwaha@gmail.com>
2020-07-29 15:08:57 +0530

    major File Structure Changes Occur in this Commit

--- DemoLibrary/AccessDemo.cs
+++ DemoLibrary/AccessDemo.cs
@@ -2,7 +2,7 @@
 
 namespace DemoLibrary
 {
-    public  class AccessLibrary
+    public  class AccessDemo
     {
         private void PrivateDemo()
         {
--- DemoLibrary/ActiveDemo.cs
+++ DemoLibrary/ActiveDemo.cs
@@ -1,9 +1,13 @@
+using System;
+
 namespace DemoLibrary
 {
     public class ActiveDemo
     {
-               
-               
+               private void MakeDemoCalls(){
+                   AccessDemo demo = new AccessDemo();
+                   
+               }
             
     }
 }
