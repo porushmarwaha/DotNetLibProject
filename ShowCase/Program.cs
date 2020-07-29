782ccf747c00f328b36956e30e9b0d66be18f498
porushmarwaha <porushmarwaha@gmail.com>
2020-07-29 15:07:56 +0530

    major File Structure Changes Occur in this Commit

--- ShowCase/Program.cs
+++ ShowCase/Program.cs
@@ -1,35 +1,12 @@
 ﻿using System;
-using UtilityLibraries;
 
-class Program
+namespace ShowCase
 {
-    static void Main(string[] args)
+    class Program
     {
-        int row = 0;
-
-        do
+        static void Main(string[] args)
         {
-            if (row == 0 || row >= 25)
-               ResetConsole();
-
-            string input = Console.ReadLine();
-            if (String.IsNullOrEmpty(input)) break;
-            Console.WriteLine($"Input: {input} {"Begins with uppercase? ",30}: " +
-                              $"{(input.StartsWithUpper() ? "Yes" : "No")}\n");
-            row += 3;
-        } while (true);
-        return;
-
-        // Declare a ResetConsole local method
-        void ResetConsole()
-        {
-            if (row > 0) {
-               Console.WriteLine("Press any key to continue...");
-               Console.ReadKey();
-            }
-            Console.Clear();
-            Console.WriteLine("\nPress <Enter> only to exit; otherwise, enter a string and press <Enter>:\n");
-            row = 3;
+            Console.WriteLine("Hello World!");
         }
     }
 }
